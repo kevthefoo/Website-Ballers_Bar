6d521da768f9785fc1acea01650c9814bf5a802c
KevTheFoo <kevthefoo@gmail.com>
2024-09-14 18:39:39 +1000

    Refactor iframe attributes in page component

--- app/page.tsx
+++ app/page.tsx
@@ -11,10 +11,9 @@ export default function Home() {
                         height="100%"
                         src="https://www.youtube.com/embed/01fz1gvHRxg?si=x-LQqOyf2A6NGcAT"
                         title="YouTube video player"
-                        frameborder="0"
                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                        referrerpolicy="strict-origin-when-cross-origin"
-                        allowfullscreen
+                        referrerPolicy="strict-origin-when-cross-origin"
+                        allowFullScreen
                     ></iframe>
                 </div>
                 <div className="grid__child border-2 border-white">video</div>
