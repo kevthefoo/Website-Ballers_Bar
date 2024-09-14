ba3cd0e9c94c8d2385922784648d705bfe020af9
KevTheFoo <kevthefoo@gmail.com>
2024-09-14 18:20:54 +1000

    Refactor Header component and update CSS styles

--- app/Components/Header/Header.tsx
+++ app/Components/Header/Header.tsx
@@ -1,9 +1,48 @@
 import React from "react";
+import { FaYoutube, FaDiscord, FaTwitter } from "react-icons/fa";
+import Link from "next/link";
 
 const Header = () => (
-  <header>
-    <h1>Header</h1>
-  </header>
+    <header className="flex fixed justify-around px-24 items-center border-b-2 border-b-white h-16 w-full">
+        <div className="h-full">
+            <h1>Ballers Bar</h1>
+        </div>
+        <nav>
+            <ul className="flex justify-around items-center gap-4">
+                <li>
+                    <Link href="/">Home</Link>
+                </li>
+                <li>
+                    <Link href="/menu">NBA</Link>
+                </li>
+                <li>
+                    <Link href="/contact">MLB</Link>
+                </li>
+                <li>
+                    <Link href="/contact">Stat</Link>
+                </li>
+            </ul>
+        </nav>
+        <div>
+            <ul className="flex justify-center items-center gap-4">
+                <li>
+                    <Link href="/login">
+                        <FaYoutube />
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/register">
+                        <FaDiscord />
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/register">
+                        <FaTwitter />
+                    </Link>
+                </li>
+            </ul>
+        </div>
+    </header>
 );
 
 export default Header;
--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,19 @@ export default function Home() {
     return (
         <section>
             <h1>NBA</h1>
-            <div className="grid_container border-2 border-red-500 grid grid-cols-5 grid-flow-row gap-2 text-center">
-                <div className="grid__child border-2 border-white">video</div>
+            <div className="grid_container border-2 border-red-500 grid grid-cols-3 grid-flow-row gap-2 text-center">
+                <div className="grid__child border-2 border-white aspect-video overflow-hidden">
+                    <iframe
+                        width="100%"
+                        height="100%"
+                        src="https://www.youtube.com/embed/01fz1gvHRxg?si=x-LQqOyf2A6NGcAT"
+                        title="YouTube video player"
+                        frameborder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                        referrerpolicy="strict-origin-when-cross-origin"
+                        allowfullscreen
+                    ></iframe>
+                </div>
                 <div className="grid__child border-2 border-white">video</div>
                 <div className="grid__child border-2 border-white">video</div>
                 <div className="grid__child border-2 border-white">video</div>
